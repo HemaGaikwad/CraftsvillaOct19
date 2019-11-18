3250bee28341beebe2e96f8264d9b83356bfa8a9
hema <hema.gowda1991@gmail.com>
2019-11-18 20:44:38 +0530

    First Commit

---
+++ src/com/craftsvilla/testcripts/LogInScripts.java
@@ -0,0 +1,47 @@
+package com.craftsvilla.testcripts;
+
+import org.testng.annotations.Test;
+
+import com.craftsvilla.generic.BaseTest;
+import com.craftsvilla.generic.ReadExcel;
+import com.craftsvilla.pompage.HomePage;
+import com.craftsvilla.pompage.SignInPage;
+
+public class LogInScripts extends BaseTest
+{
+	HomePage homepage = null;
+	SignInPage signInpage = null;
+	
+	@Test
+	public void TC_01_login()
+	{
+		homepage = new HomePage(driver);
+		signInpage = new SignInPage(driver);
+		
+		
+		try
+		{
+			String[][] credentials = ReadExcel.getData(FILE_PATH, "Sheet1");
+			for(int i = 1; i<credentials.length; i++)
+			{
+				String email = credentials[i][0];
+				String password = credentials[i][1];
+				
+				homepage.clickOnSignInLink();
+				signInpage.setEmail(email);
+				signInpage.clickOnContinue();
+				signInpage.enterPassword(password);
+				signInpage.clickOnLoginButton();
+				homepage.gotoAccount();
+				homepage.clickOnLogout();
+			}
+			
+			System.out.println();
+		}
+		catch(Exception e)
+		{
+			
+		}
+	}
+
+}
